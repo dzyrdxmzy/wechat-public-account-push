1e946b8fd35eb1bdcc6bf37c1250a8597c1024a5
dzyrdxmzy <115187289+dzyrdxmzy@users.noreply.github.com>
2025-06-25 21:21:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWR05vjW1OTekNBsuGbD0XJXvfek',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '5imPdvWp-ZalOzLos1L05Kw716eP8afJ3sfwj9CvQVc',
+      useTemplateId: 'x0FM7rWyda0jc8-MNWyiFbLOGeWury-e2D5M7NNewWU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-20',
       festivals: [
