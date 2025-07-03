db1a87ae0e270daa2d6c8d717d3af63166e40b61
dzyrdxmzy <115187289+dzyrdxmzy@users.noreply.github.com>
2025-07-03 13:17:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oWR05vrqZtGp8vVGZcLT61XNC0i0',
+      id: 'oWR05vrqZtGp8vVGZcLT61XNC0i',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'WrHR-7cGnWAmqgVj0pYbYJGYL8oCa1dl849ku3MwvmE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -46,7 +46,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWR05vjW1OTekNBsuGbD0XJXvfek',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'WrHR-7cGnWAmqgVj0pYbYJGYL8oCa1dl849ku3MwvmE',
+      useTemplateId: 'jMdz4FYzaOY__JMOt4voa8KfcEKgzJKbTzpIA4NF3w4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-29',
       festivals: [
