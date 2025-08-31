ae53f8fa00ed0b147ade1006fe305fa04a4b82f5
dzyrdxmzy <115187289+dzyrdxmzy@users.noreply.github.com>
2025-08-31 14:24:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '9',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWR05vrqZtGp8vVGZcLT61XNC0i0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
