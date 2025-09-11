4ad6351396f55c14f6f832b83f9afbd8cc1391cc
dzyrdxmzy <115187289+dzyrdxmzy@users.noreply.github.com>
2025-09-12 02:00:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,15 +13,15 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '2a6aa74a415eda7e6b80bb3b1588ba57',
 
-  PROVINCE: '江苏',
-  CITY: '南京',
+  PROVINCE: '香港',
+  CITY: '香港',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '萱',
+      name: '泽',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oWR05vjH9HxTE4WDVNkWKE3AJxTs',
+      id: 'oWR05vgpRA_oS84ZdUze8O8RjU_',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'AKSBz9R6WdvxuilyOwbifRjd5-vBUjC-Q4rxdY4QQ0c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -29,13 +29,13 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: 'you', year: '2000', date: '10-9',
+          type: '生日', name: 'you', year: '2004', date: '8-7',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2025-08-25' },
+        { keyword: 'love_day', date: '2025-08-27' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -52,13 +52,13 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: 'you', year: '2000', date: '10-9',
+          type: '生日', name: 'you', year: '2004', date: '8-7',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2025-08-25' },
+        { keyword: 'love_day', date: '2025-08-27' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
