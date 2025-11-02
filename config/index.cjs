79643d241f82fc5d95dd08f8d5bbfd83c71c8863
dzyrdxmzy <115187289+dzyrdxmzy@users.noreply.github.com>
2025-11-03 00:19:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '林深',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oWR05vuzi_xJh5tzIzbCUGbLCli',
+      id: 'oWR05vuzi_xJh5tzIzbCUGbLCliM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'LOnPIJ3KRLTqhpx4991ZiTrfDkY1gjngVKwxAmrCf00',
       
