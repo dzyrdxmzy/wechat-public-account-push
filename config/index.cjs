9e797955104122970e4f14c9c3fdd2ec2e88e297
dzyrdxmzy <115187289+dzyrdxmzy@users.noreply.github.com>
2025-11-28 23:24:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -30,7 +30,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '老大', year: '2000', date: '05-09',
+          type: '生日', name: '宝宝', year: '2000', date: '05-09',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -54,7 +54,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '老大', year: '2000', date: '05-09',
+          type: '生日', name: '宝宝', year: '2000', date: '05-09',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
